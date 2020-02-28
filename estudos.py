16b6c709431fedb7367e6d184f69ae22d57880a6
Eduardo Máximo Cano <e.maximocano@gmail.com>
2020-02-27 21:05:29 -0300

    segundo tumulo

--- estudos.py
+++ estudos.py
@@ -6,5 +6,11 @@ peso = 78
 tumulo_01 = (nome, idade, altura, peso)
 print (tumulo_01)
 
+nome = "Maria"
+idade = 19
+altura = 1,20
+peso = 59
 
+tumulo_02 = (nome, idade, altura, peso)]
+print (tumulo_02)
 
