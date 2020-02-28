25f43f03a8873ca443d329b5e856f10bad0a818a
Eduardo Máximo Cano <e.maximocano@gmail.com>
2020-02-27 22:28:08 -0300

    ajusta tumulo

--- estudos.py
+++ estudos.py
@@ -1,16 +1,16 @@
-nome = "Jose"
-idade = 22  
-altura = 1,10
-peso = 78
-
-tumulo_01 = (nome, idade, altura, peso)
+tumulo_01 = {
+    "nome": "Jose",
+    "idade": 22,
+    "altura": 1.10,
+    "peso": 78
+     }
 print (tumulo_01)
 
-nome = "Maria"
-idade = 19
-altura = 1,20
-peso = 59
-
-tumulo_02 = (nome, idade, altura, peso)
+tumulo_02 = {
+    "nome": "Maria", 
+    "idade": 19, 
+    "altura": 1.20, 
+    "peso": 59
+    }
 print (tumulo_02)
 
